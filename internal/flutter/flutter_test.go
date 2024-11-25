45040feffd2a81bf0e4e6c106b72b57429837ef7
law <lawdanazoumi@gmail.com>
2024-11-25 21:09:25 +0000

    fix in flutter_test.go

--- internal/flutter/flutter_test.go
+++ internal/flutter/flutter_test.go
@@ -1,20 +1,2 @@
 package flutter
 
-import (
-	"testing"
-)
-
-func TestRunForFlutter(t *testing.T) {
-	files := []string{}
-	inputLang := "en"
-
-	err := RunForFlutter(&files, &inputLang)
-	if err != nil {
-		t.Errorf("RunForFlutter failed: %v", err)
-	}
-
-	if len(files) == 0 {
-		t.Errorf("No files were parsed for the Flutter project")
-	}
-}
-
